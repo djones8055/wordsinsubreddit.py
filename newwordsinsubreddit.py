175009ba091430fa85b003b0f126f1b754d8ee83
djones8055 <31707342+djones8055@users.noreply.github.com>
2017-09-17 12:02:23 -0400

    Update newwordsinsubreddit.py
    made everything check out with pylint

--- newwordsinsubreddit.py
+++ newwordsinsubreddit.py
@@ -1,85 +1,91 @@
+""" a simple python program to count the wourds in a subreddits posts
+	there should be a praw.ini file in the same dir as this program
+    leave the /r/ off of the reddit name, it is added by the script"""
+
+#!/usr/bin/env python3
 import csv
 import os
-import praw
-import string
 import unicodedata
 import sys
+import praw
 
 # function for setting up praw
 
 def setuppraw():
-	
-	reddit = praw.Reddit('bot1')
-	subreddit = reddit.subreddit(input('what sub to count? '))
-	print()
-	print('counting words in /r/' + str(subreddit))
-	
-	return subreddit
+    """ Looks for praw.ini and use it to set up praw."""
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit(input('what sub to count? '))
+    print()
+    print('counting words in /r/' + str(subreddit))
 
-# function for checking if a file containing a word count already 
+    return subreddit
+
+# function for checking if a file containing a word count already
 # exists in the same dir as this script
 
 def loadfile():
-	
-	if not os.path.isfile('foundwords.csv'):
-		founddata = []
-		print('Made a new foundwords.csv')
-		print()
-	
-	else:
-		infile = open('foundwords.csv')
-		inreader = csv.reader(infile)
-		founddata = list(inreader)
-		print('using existing foundwords.csv')
-		print()
-	return founddata
-	
+    """ Look for the found words csv file, if there is one load it
+        if there isnt one, make it. """
+    if not os.path.isfile('foundwords.csv'):
+        founddata = []
+        print('Made a new foundwords.csv')
+        print()
+
+    else:
+        infile = open('foundwords.csv')
+        inreader = csv.reader(infile)
+        founddata = list(inreader)
+        print('using existing foundwords.csv')
+        print()
+    return founddata
+
 # this code removes the punctuation from the words that are found
 
-tbl = dict.fromkeys(i for i in range(sys.maxunicode)
-if unicodedata.category(chr(i)).startswith('P'))
-                      
+TBL = dict.fromkeys(i for i in range(sys.maxunicode)
+                    if unicodedata.category(chr(i)).startswith('P'))
+
 def removepunctuation(text):
-    return text.translate(tbl)
-    
-# function for making a list out of each post to check against the 
-# words already in the word count file 
-  
+    """ Return the word minus the punctuation."""
+    return text.translate(TBL)
+
+# function for making a list out of each post to check against the
+# words already in the word count file
+
 def loadpost():
-	postwords = []
-	subreddit = setuppraw()
-	howmany = input('How many posts should I read? ')
-	for submission in subreddit.hot(limit=int(howmany)):
-		for word in submission.selftext.split():
-			word = removepunctuation(word)
-			word = word.lower()
-			postwords.append(word)
-	return postwords
-	
+    """ Return a list of the words in the posts self text. """
+    postwords = []
+    subreddit = setuppraw()
+    howmany = input('How many posts should I read? ')
+    for submission in subreddit.hot(limit=int(howmany)):
+        for word in submission.selftext.split():
+            word = removepunctuation(word)
+            word = word.lower()
+            postwords.append(word)
+    return postwords
+
 def postvsfile(postlist, filelist):
-	for word in postlist:
-		if word not in [item[0] for item in filelist]:
-			filelist.append([word,'1'])
-			print("added " + word + " to list")
-		else:
-			index = int([item[0] for item in filelist].index(word))
-			filelist[index][1] = int(filelist[index][1]) + 1
-			
-			
-	return filelist
-	
+    """ Compair the words in the postlist with the words in the filelist
+    if the word is already in the file list, load its point value and
+    add one to it."""
+    for word in postlist:
+        if word not in [item[0] for item in filelist]:
+            filelist.append([word, '1'])
+            print("added " + word + " to list")
+        else:
+            index = int([item[0] for item in filelist].index(word))
+            filelist[index][1] = int(filelist[index][1]) + 1
+
+    return filelist
+
 def writefile(founddata):
-	outfile = open('foundwords.csv', 'w', newline = '')
-	outwriter = csv.writer(outfile)
-	
-	for row in founddata:
-		outwriter.writerow(row)
-	outfile.close
+    """ Write the updated filelist list to the foundwords.csv file."""
+    outfile = open('foundwords.csv', 'w', newline='')
+    outwriter = csv.writer(outfile)
 
+    for row in founddata:
+        outwriter.writerow(row)
 
-filedata = postvsfile(loadpost(), loadfile())
-print('Words in csv file: ', len(filedata))
+FILE_WORDS = postvsfile(loadpost(), loadfile())
+print('Words in csv file: ', len(FILE_WORDS))
 
-writefile(filedata)
-	
-	
+writefile(FILE_WORDS)
